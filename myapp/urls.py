6882ba6d09978d83e728496bedfe022aa471016b
unknown <your_email@example.com>
2025-04-23 21:07:14 +0330

    mmd

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,4 @@
-from django.views.generic import TemplateView
-
-urlpatterns += [
-    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
 ]
